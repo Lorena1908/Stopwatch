b00696a6592721f1ae2bf065640f3bccff37a119
Lorena <lore.lo2310@gmail.com>
2021-06-04 21:45:26 -0300

    Stopwatch working with seconds, minutes, hours and days

--- main.py
+++ main.py
@@ -40,47 +40,79 @@ class Button:
                 return True
         return False
 
-def draw_window(play_pause_btn, reset_btn, time_count):
+def draw_window():
     pygame.draw.circle(win, (252,252,252), (width/2, height/2-80), radius)
     pygame.draw.circle(win, (32, 33, 36), (width/2, height/2-80), radius-3)
     play_pause_btn.draw()
     reset_btn.draw()
 
     # time
-    font1 = pygame.font.SysFont('comicsans', 60)
-    font2 = pygame.font.SysFont('comicsans', 30)
-    
-    if time_count == 0:
-        seconds = font1.render('0', 1, (138, 180, 248))
-    elif time_count > 0:
-        seconds = font1.render(str(time_count-1), 1, (138,180,248))
-    miliseconds = font2.render('00', 1, (138, 180, 248))
-    win.blit(seconds, (width/2 - seconds.get_width()/2, height/2-80 - seconds.get_height()/2))
-    # win.blit(miliseconds, (width/2 - miliseconds.get_width()/2 + miliseconds.get_width() + 5, height/2-80 - miliseconds.get_height()/2 + 7))
+    font1 = pygame.font.SysFont('comicsans', 40)
 
-    pygame.display.update()
+    if second == -1 and minute == 0 and hour == 0 and day == 0:
+        time = font1.render(f'0', 1, (138, 180, 248))
+    elif second >= 0:
+        time = font1.render(f'{second}', 1, (138, 180, 248))
+        if minute == 0 and hour == 0 and day == 0:
+            time = font1.render(f'{second}', 1, (138, 180, 248))
+        elif minute > 0 and hour == 0 and day == 0:
+            time = font1.render(f'{minute}:{second}', 1, (138, 180, 248))
+        elif hour > 0 and day == 0:
+            time = font1.render(f'{hour}:{minute}:{second}', 1, (138, 180, 248))
+        elif day > 0:
+            time = font1.render(f'{day}:{hour}:{minute}:{second}', 1, (138, 180, 248))
+
+    win.blit(time, (width/2 - time.get_width()/2, height/2-80 - time.get_height()/2))
 
 def main():
+    global second, minute, hour, play_pause_btn, reset_btn, day
     play_pause_btn = Button(width/2, height/2+110, (138, 180, 248), 'play_pause')
     reset_btn = Button(width/2-90, height/2+105, (252,252,252), 'reset')
     run = True
     clock = pygame.time.Clock()
     time = 0
     needed_time = 1000 # In milisseconds
-    time_count = 0
     start_counting = False
 
+    # Time
+    second = -1
+    minute = 0
+    hour = 0
+    day = 0
+
+    # Seconds explanation
+    '''
+    The seconds in lines 84 and 131 need to start at -1 because the clock.tick() gets the time between 
+    when the program started to run and now, so if I run the stopwatch file and wait more than one second 
+    to click the play button the clock.get_rawtime() will add more than 1000 milisseconds to time. This 
+    means that time >= needed_time and start_count will evaluate to True and thus it'll add 1 to time. 
+    To revert that, I made the seconds start at -1 so when 1 is added it'll be 0 and start as a normal 
+    stopwatch.
+
+    To hide this -1 from the stopwatch interface I made a simple if statement at the draw_window() 
+    function (line52).
+    '''
+
     while run:
+        clock.tick()
+        time += clock.get_rawtime()
         win.fill((32, 33, 36))
-        draw_window(play_pause_btn, reset_btn, time_count)
+        draw_window()
 
-        if start_counting:
-            time += clock.get_rawtime()
-            clock.tick()
-
-            if time >= needed_time and start_counting:
-                time = 0
-                time_count += 1
+        if time >= needed_time and start_counting:
+            time = 0
+            second += 1
+            if second == 60:
+                second = 0
+                minute += 1
+            
+            if minute == 60:
+                minute = 0
+                hour += 1
+            
+            if hour == 24:
+                hour = 0
+                day += 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -104,7 +136,9 @@ def main():
                 elif reset_btn.clicked(x, y):
                     play_pause_btn.click = False
                     start_counting = False
-                    time_count = 0
+                    second = -1
+                    minute = 0
+                    hour = 0
         
         pygame.display.update()
 
